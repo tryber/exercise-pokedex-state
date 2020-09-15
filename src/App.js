51a0ddb9bd6da5d1cc827ba36ebbd1431e48bc73
Ana Flávia <anaflvia@gmail.com>
2020-09-15 20:07:36 -0300

    Iniciando

--- src/App.js
+++ src/App.js
@@ -3,13 +3,28 @@ import './App.css';
 import pokemons from './data';
 import Pokedex from './Pokedex';
 
-function App() {
-  return (
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      index: 0
+    }
+  }
+  handleClick() {
+    while(this.state.index < pokemons.length) {
+      this.setState((estadoAnterior, _props) => ({
+        index: estadoAnterior.index + 1
+      }))
+    }
+  }
+  render() {
+    return (
     <div className="App">
       <h1> Pokedex </h1>
-      <Pokedex pokemons={pokemons} />
+      <Pokedex pokemon={pokemons[this.state.index]} />
+      <button onClick={this.handleClick}>Próximo Pokemon</button>
     </div>
-  );
+  )}
 }
 
 export default App;
--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import pokemons from './data';
 import Pokemon from './Pokemon';
 
 class Pokedex extends React.Component {
     render() {
         return (
             <div className="pokedex">
-                {this.props.pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
+                <Pokemon key={pokemon.id} name={pokemon.name} type={pokemon.type} averageWeight={pokemon.averageWeight} image={pokemon.image} />
+                {/* {this.props.pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)} */}
             </div>
         );
     }
