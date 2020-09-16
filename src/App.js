719c4447243347dd5022ade68a7b7f2ad9fca28d
Ana Flávia <anaflvia@gmail.com>
2020-09-16 09:36:13 -0300

    atualizando

--- src/App.js
+++ src/App.js
@@ -4,31 +4,11 @@ import pokemons from './data';
 import Pokedex from './Pokedex';
 
 class App extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      index: 0
-    }
-  }
-  handleClick() {
-    while(this.state.index < pokemons.length) {
-      if (this.state.index === pokemons.length - 1) {
-        this.setState((estadoAnterior, _props) => ({
-          index: 0
-        }))
-      } else {
-        this.setState((estadoAnterior, _props) => ({
-          index: estadoAnterior.index + 1
-        }))
-      }
-    }
-  }
   render() {
     return (
     <div className="App">
       <h1> Pokedex </h1>
-      <Pokedex pokemon={pokemons[this.state.index]} />
-      <button onClick={this.handleClick}>Próximo Pokemon</button>
+      <Pokedex />
     </div>
   )}
 }
--- src/Pokedex.js
+++ src/Pokedex.js
@@ -3,11 +3,31 @@ import pokemons from './data';
 import Pokemon from './Pokemon';
 
 class Pokedex extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+          index: 0
+        }
+      }
+      handleClick() {
+        while(this.state.index < pokemons.length) {
+          if (this.state.index === pokemons.length - 1) {
+            this.setState((estadoAnterior, _props) => ({
+              index: 0
+            }))
+          } else {
+            this.setState((estadoAnterior, _props) => ({
+              index: estadoAnterior.index + 1
+            }))
+          }
+        }
+      }
     render() {
+        const { pokemon } = this.props
         return (
             <div className="pokedex">
-                <Pokemon key={this.props.pokemon.id} name={this.props.pokemon.name} type={this.props.pokemon.type} averageWeight={this.props.pokemon.averageWeight} image={this.props.pokemon.image} />
-                {/* {this.props.pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)} */}
+                <Pokemon key={pokemons[this.state.index].id} name={pokemons[this.state.index].name} type={pokemons[this.state.index].type} averageWeight={pokemons[this.state.index].averageWeight} image={pokemons[this.state.index].image} />
+                <button onClick={this.handleClick}>Próximo Pokemon</button>
             </div>
         );
     }
