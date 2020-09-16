5878afbda5c5c5c7e8a4fb6092da1256862d74b6
Pedro Risso <pmerchiorattor@gmail.com>
2020-09-16 15:44:21 -0300

    pokemons are rendered one by one but pokemon type filter needs work

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -2,13 +2,56 @@ import React from 'react';
 import Pokemon from './Pokemon';
 
 class Pokedex extends React.Component {
-    render() {
-        return (
-            <div className="pokedex">
-                {this.props.pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
-            </div>
-        );
+  constructor(props) {
+    super();
+    this.changePokemon = this.changePokemon.bind(this)
+    this.getTypes = this.getTypes.bind(this)
+    this.state = {
+      pokeIndex: 0,
+      filteredPokes: props.pokemons.reduce((acc, current) => {
+      if (current.type in acc) {
+        console.log(current.type)
+        return acc.push(current.type)
+      }
+      //console.log(current.type)
+      //console.log(acc)
+      return acc
+      /* (current.type in acc ? acc.push(current.type) : acc), ['All']  */
+      }
+    , ['All'])
     }
+  }
+
+  getTypes() {
+    this.props.pokemons.reduce((acc, current) =>
+      (this.type in acc ? acc.push(current.type) : acc), ['All']
+    )
+  }
+
+  changePokemon() {
+    this.state.pokeIndex < this.props.pokemons.length - 1
+      ? this.setState((previous, _props) => ({
+        pokeIndex: previous.pokeIndex + 1
+      }))
+      : this.setState({ pokeIndex: 0 })
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="pokedex">
+          {this.props.pokemons.map((pokemon) => {
+            return <Pokemon key={pokemon.id} pokemon={pokemon} />
+          })[this.state.pokeIndex]}
+        </div>
+        <button onClick={this.changePokemon}>Next Pokemon</button>
+        <div className="pokeType">
+          {this.state.filteredPokes.map(pokeType =>
+            <button key="">{pokeType}</button>)}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Pokedex;
--- src/Pokemon.js
+++ src/Pokemon.js
@@ -3,7 +3,6 @@ import React from 'react';
 class Pokemon extends React.Component {
     render() {
         const { name, type, averageWeight, image } = this.props.pokemon;
-
         return (
             <div className="pokemon">
                 <div>
