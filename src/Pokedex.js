6297b2ed99ef446e11cc2777cfa8ae8c0607abf0
Pedro Risso <pmerchiorattor@gmail.com>
2020-09-16 19:12:25 -0300

    pokemon filter type added

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -8,23 +8,28 @@ class Pokedex extends React.Component {
     this.filterTypes = this.filterTypes.bind(this)
     this.state = {
       pokeIndex: 0,
-      filteredPokes: props.pokemons.reduce((acc, current) =>
+      pokeTypes: props.pokemons.reduce((acc, current) =>
         acc.includes(current.type) ? acc : acc.concat(current.type),
-        ['All'])
+        ['All']),
+      currentPokeList: props.pokemons,
     }
   }
 
   filterTypes(chosenType) {
-    return this.props.pokemons.filter(poke => {
-      if (poke.type === chosenType) {
-        return poke
-      }
-    }
-    )
+    chosenType === 'All'
+      ? this.setState(_prev => ({ currentPokeList: this.props.pokemons }))
+      : this.setState(_prev => ({
+        currentPokeList: this.props.pokemons.filter(function (poke) {
+          return poke.type === chosenType
+        }
+        )
+      })
+      )
+    this.setState({ pokeIndex: 0 })
   }
 
   changePokemon() {
-    this.state.pokeIndex < this.props.pokemons.length - 1
+    this.state.pokeIndex < this.state.currentPokeList.length - 1
       ? this.setState((previous, _props) => ({
         pokeIndex: previous.pokeIndex + 1
       }))
@@ -35,14 +40,18 @@ class Pokedex extends React.Component {
     return (
       <div>
         <div className="pokedex">
-          {this.props.pokemons.map((pokemon) => {
+          {this.state.currentPokeList.map((pokemon) => {
             return <Pokemon key={pokemon.id} pokemon={pokemon} />
           })[this.state.pokeIndex]}
         </div>
         <button onClick={this.changePokemon}>Next Pokemon</button>
         <div className="pokeType">
-          {this.state.filteredPokes.map(pokeType =>
-            <button key={pokeType}>{pokeType}</button>)}
+          {this.state.pokeTypes.map(pokeType =>
+            <button
+              key={pokeType}
+              onClick={() => this.filterTypes(pokeType)}>
+              {pokeType}
+            </button>)}
         </div>
       </div>
     );
