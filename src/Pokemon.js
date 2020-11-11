51b95ac52ff978e3cebad36ca12ee61607e836f4
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2020-11-11 20:15:59 -0300

    Adicionado um pouco de css

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import './pokemon.css'
 class Pokemon extends React.Component {
     render() {
         const { name, type, averageWeight, image } = this.props.pokemon;
 
         return (
-            <div className="pokemon">
+            <div className={`pokemon ${type.toLowerCase()}`}>
                 <div>
                     <p> {name} </p>
                     <p> {type} </p>
