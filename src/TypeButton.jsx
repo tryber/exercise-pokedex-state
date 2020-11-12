cde8ed1fdb2b502b596f6be957c1f441fc57159c
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2020-11-11 22:48:40 -0300

    Adicionada referencia do reduce

--- src/TypeButton.jsx
+++ src/TypeButton.jsx
@@ -5,6 +5,8 @@ class TypeButtons extends Component {
         const { pokemonType, pokemons } = this.props;
         const types = pokemons.map(pokemon => pokemon.type)
                               .reduce((unique, item) => unique.includes(item) ? unique:[...unique, item],[])
+                              //reduce feito utilizando tutorial desse site
+                              //https://medium.com/dailyjs/how-to-remove-array-duplicates-in-es6-5daa8789641c
         return (
             <div>
                 {
