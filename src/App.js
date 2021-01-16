a87a736f78737da2b476825e27d9c5cfc52787b1
Seu nome <luiza.dilly@gmail.com>
2021-01-16 15:22:52 -0300

    adiciona logo

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ import Pokedex from './Pokedex';
 function App() {
   return (
     <div className="App">
-      <h1> Pokedex </h1>
+      <h1>
+        <img src="https://1000logos.net/wp-content/uploads/2017/05/Pokemon_logo_PNG2.png" alt="pokemonLogo" className="pokeLogo"></img>
+      </h1>
       <Pokedex pokemons={pokemons} />
     </div>
   );
