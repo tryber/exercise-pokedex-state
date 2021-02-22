ec4950d9d29d73f0e8e5d50731d82e3561e6211d
johnealves <johne_af@hotmail.com>
2021-02-22 17:03:21 -0300

    fix height info and box info

--- src/App.js
+++ src/App.js
@@ -2,16 +2,12 @@ import React, { Component } from 'react'
 import './App.css';
 import pokemons from './data'
 import Pokedex from './Pokedex'
-// import Pokemon from './Pokemon';
-// import PokemonAlone from './PokemomAlone';
-// import { FaArrowRight } from 'react-icons/fa'
-// import { FaArrowLeft } from 'react-icons/fa'
 
 class App extends Component {
   render() {
     return (
       <div>
-          <Pokedex pokemons={ pokemons }/>
+          <Pokedex pokemons={ pokemons } />
       </div>
     );
   }
