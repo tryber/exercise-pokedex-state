b737c96924a722d0908856b5296567648947afbc
palenske <viniciuspalenske@gmail.com>
2021-02-23 16:39:57 -0300

    apresentando pokemons um a um através do evendo de click

---
+++ src/Components/Pokedex.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Pokemon from './Pokemon';
+class Pokedex extends React.Component {
+  constructor(props) {
+    super(props);
+    this.upIndex = this.upIndex.bind(this);
+    this.downIndex = this.downIndex.bind(this);
+    this.state = {
+      index: 0,
+      pokemonList: this.props.pokemons.map(pokemon => pokemon)
+    };
+  }
+  
+  upIndex() {
+    const { index, pokemonList} = this.state;
+    if (index === pokemonList.length -1) {
+      this.setState(() => ({index: 0}))
+    } else this.setState((prev) => ({index: prev.index + 1}))
+  }
+
+  downIndex() {
+    const { index, pokemonList } = this.state;
+    if (index === 0) {
+      this.setState(() => ({index: pokemonList.length - 1}))
+    } else this.setState((prev) => ({index: prev.index - 1}))
+  }
+
+  render() {
+    const { pokemonList, index} = this.state;
+    return (
+      <div className="pokedex">
+        <button onClick={this.downIndex}>Prev</button>
+        { <Pokemon key={pokemonList[index].id} pokemon={pokemonList[index]} /> }
+        <button onClick={this.upIndex}>Next</button>
+      </div>
+    );
+  }
+}
+
+export default Pokedex;
