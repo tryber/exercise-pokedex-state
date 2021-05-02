30f7baeb77af3d6d7f9c859fc733899f55d8bddd
DennisMarcelo <dennismarcelo15@gmail.com>
2021-05-02 11:34:07 -0300

    my-pokedex_dark_mode

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import '../App.css';
+import PokeDex from './Pokedex';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>PokeDex</h1>
+      <PokeDex />
+    </div>
+  );
+}
+
+export default App;
