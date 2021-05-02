b38dd72e7d382bfa77f3a687f10c228eca45d1f2
Aremtior <marcosleandrofl@gmail.com>
2021-05-02 01:03:39 -0300

    exercise 2 - filter was used to show only fire and psychic pokemons.

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -5,24 +5,37 @@ class Pokedex extends React.Component {
     constructor(props) {
         super();
         this.state = {
+            nextType: 'Psychic',
             next: 0,
         }
     }
 
-    handleClick = () => {
-        this.setState((prevState, _props) => ({
-            next: this.props.pokemons.length - 1 === prevState.next ? 0 : prevState.next + 1
-        }));
+    handleClick = ({ target: { name, value } }) => {
+        const len = value - 1;
+        this.setState((prevState, _props) => {
+            const changeState = len === prevState.next || name !== prevState.nextType ? 0 : prevState.next + 1;
+            return {
+                nextType: name,
+                next: changeState
+            }
+        });
+    }
+
+    filterMethod = (type) => {
+        return this.props.pokemons.filter((pokemon) => pokemon.type === type);
     }
 
     render() {
+        const { nextType, next } = this.state;
+        const poke = nextType === 'Fire' ? this.filterMethod('Fire') : this.filterMethod('Psychic');
         return (
             <div>
                 <div className="pokedex">
                     {/* {this.props.pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)} */}
-                    <Pokemon key={this.props.pokemons[this.state.next].id} pokemon={this.props.pokemons[this.state.next]} />
+                    <Pokemon key={poke[next].id} pokemon={poke[next]} />
                 </div>
-                <button onClick={this.handleClick}>Next</button>
+                <button name="Fire" value={poke.length} onClick={this.handleClick}>Next Fire</button>
+                <button name="Psychic" value={poke.length} onClick={this.handleClick}>Next Psychic</button>
             </div>
         );
     }
