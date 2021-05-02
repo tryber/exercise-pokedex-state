57eee22ec9872f416dc750a9aa2832632059c5de
Gustavo Sousa <gustavohl98@gmail.com>
2021-05-02 16:18:15 -0300

    Exercicios 1 - Implementa exibicao de 1 pokemon por vez

--- src/App.js
+++ src/App.js
@@ -3,13 +3,40 @@ import './App.css';
 import pokemons from './data';
 import Pokedex from './Pokedex';
 
-function App() {
-  return (
-    <div className="App">
-      <h1> Pokedex </h1>
-      <Pokedex pokemons={pokemons} />
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      currentPokemon: 0,
+    }
+
+  }
+
+  handleClick() {
+    
+    this.setState((previousState, _props) => {
+      if (previousState.currentPokemon === pokemons.length - 1) {
+        return ({
+          currentPokemon: 0,
+        })
+      };
+
+      return ({
+        currentPokemon: previousState.currentPokemon + 1,
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1> Pokedex </h1>
+        <Pokedex pokemons={pokemons.filter((pokemon) => pokemons.indexOf(pokemon) === this.state.currentPokemon)} />
+        <button onClick={ this.handleClick }>Next Pokemon</button>
+      </div>
+    );
+  }
 }
 
 export default App;
