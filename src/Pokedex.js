88325ffe59142b3b8411ccbdd7413abfbce60bde
Gabriel <gabrielfernandessilva.ba@gmail.com>
2021-05-27 15:35:25 -0300

    Exercicio 1- Aparecer apenas um pokemon por vez e botão next

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -2,11 +2,28 @@ import React from 'react';
 import Pokemon from './Pokemon';
 
 class Pokedex extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            next: 0,
+        }
+        this.nextButtonFunc = this.nextButtonFunc.bind(this);
+    }
+
+    nextButtonFunc = () => {
+        console.log(this.props.pokemons)
+        this.setState((prevState, _props) => ({
+            next: this.props.pokemons.length - 1 === prevState.next ? 0 : prevState.next + 1
+        }));
+    }
     render() {
         return (
+        <div>
             <div className="pokedex">
-                {this.props.pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
+                <Pokemon key={this.props.pokemons.name} pokemon={this.props.pokemons[this.state.next]} />
             </div>
+            <button onClick={this.nextButtonFunc}>Next</button>
+        </div>
         );
     }
 }
