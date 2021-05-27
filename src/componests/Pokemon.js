5f9e00c42a9f254b2e6546f98eeb87c6966674f7
Julio Filizzola <juliofilizzola@hotmail.com>
2021-05-27 19:54:47 -0300

    [julioFilizzola_atividade] editando arquivo Pokemon

--- src/componests/Pokemon.js
+++ src/componests/Pokemon.js
@@ -2,11 +2,16 @@ import React, { Component } from 'react'
 
 class Pokemon extends Component {
   render() {
-    const { id, image, type, image, moreInfo, averageWeight } = this.props;
+    const { id, image, type, name, moreInfo, averageWeight } = this.props;
     return (
       <div>
         <div>
-          <p>{name}</p>
+          <h1>{name}</h1>
+          <img src={image} alt={name} />
+          <p>{id}</p>
+          <p>{type}</p>
+          <p>{averageWeight.value} {averageWeight.measurementUnit}</p>
+          <p>{moreInfo}</p>
         </div>
       </div>
     )
