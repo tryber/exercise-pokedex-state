8c634438c4646f0cb8aa3b59fc663fdf0deb4a42
Arthur Hermann <arthurhermannc@gmail.com>
2021-05-28 11:40:52 -0300

    Botoes que filtram o poke

--- src/App.js
+++ src/App.js
@@ -7,12 +7,17 @@ class App extends Component {
   constructor() {
     super();
     this.nextIndex = this.nextIndex.bind(this)
+    this.AllPokes = this.AllPokes.bind(this)
+    this.OnlyBugs = this.OnlyBugs.bind(this)
+    this.OnlyFire = this.OnlyFire.bind(this)
+    this.OnlyPsychic = this.OnlyPsychic.bind(this)
     this.state = {
+      currentData: pokemonsData,
       pokeIndex: 0,
     }
   }
   nextIndex () {
-    if(this.state.pokeIndex === pokemonsData.length -1) {
+    if(this.state.pokeIndex === this.state.currentData.length -1) {
       this.setState(_ => ({
         pokeIndex: 0
       }))
@@ -22,12 +27,44 @@ class App extends Component {
       }))
     }
   }
+  AllPokes () {
+    this.setState((_ => ({
+      currentData: pokemonsData,
+      pokeIndex: 0,
+    })))
+  }
+  OnlyBugs () {
+    const typeData = pokemonsData.filter((pokemon) => pokemon.type === 'Bug')
+    this.setState((_ => ({
+      currentData: typeData,
+      pokeIndex: 0,
+    })))
+  }
+  OnlyFire () {
+    const typeData = pokemonsData.filter((pokemon) => pokemon.type === 'Fire')
+    this.setState((_ => ({
+      currentData: typeData,
+      pokeIndex: 0,
+    })))
+  }
+  OnlyPsychic () {
+    const typeData = pokemonsData.filter((pokemon) => pokemon.type === 'Psychic')
+    this.setState((_ => ({
+      currentData: typeData,
+      pokeIndex: 0,
+    })))
+  }
   render () {
     return (
       <div className="App">
         <h1> Pokedex </h1>
-        <Pokedex pokemons={pokemonsData[this.state.pokeIndex]} />
+        <Pokedex pokemons={this.state.currentData[this.state.pokeIndex]} />
         <button onClick={this.nextIndex }>Next</button>
+        <button onClick={this.AllPokes }>All</button>
+        <button onClick={this.OnlyBugs }>Bugs</button>
+        <button onClick={this.OnlyFire }>Fire</button>
+        <button onClick={this.OnlyPsychic }>Psychic</button>
+        
       </div>
     );
   }
