94f32b0b876b1e970e9e1a8fd87a74440309c828
Julio Filizzola <juliofilizzola@hotmail.com>
2021-05-28 11:43:43 -0300

    [julioFilizzola_atividade] edit arquivo app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
-
+import Data from './data';
+import Pokedex from './';
 
 function App() {
   return (
     <div className="App">
-     
+      <Pokedex pokemon={ Data }/>
     </div>
   );
 }
