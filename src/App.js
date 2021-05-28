2b8250bb54d0c732c168c80a25e8da776cb3475c
Julio Filizzola <juliofilizzola@hotmail.com>
2021-05-28 11:37:48 -0300

    [exercises/12.2] zerando e refazendo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
-import Data from './data';
-import Pokedex from './Pokedex';
+
 
 function App() {
   return (
     <div className="App">
-      <h1> Pokedex </h1>
-      <Pokedex Pokedex={ Data } />
+     
     </div>
   );
 }
