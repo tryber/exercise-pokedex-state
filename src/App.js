ec449607edd27e35ad2cc3cc29703524f661eb62
Eric Kreis <erickreis793@gmail.com>
2021-06-23 15:09:52 -0300

    create next pokemon button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
 import pokemons from './data';
-import Pokedex from './Pokedex';
+import Pokemon from './Pokemon';
 
 function App() {
   return (
     <div className="App">
       <h1> Pokedex </h1>
-      <Pokedex pokemons={pokemons} />
+      <Pokemon pokemons={pokemons} />
     </div>
   );
 }
--- src/Pokemon.js
+++ src/Pokemon.js
@@ -1,20 +1,51 @@
 import React from 'react';
 
 class Pokemon extends React.Component {
-    render() {
-        const { name, type, averageWeight, image } = this.props.pokemon;
-
-        return (
-            <div className="pokemon">
-                <div>
-                    <p> {name} </p>
-                    <p> {type} </p>
-                    <p> {`Average weight: ${averageWeight.value} ${averageWeight.measurementUnit}`}</p>
-                </div>
-                <img src={image} alt={`${name} sprite`} />
-            </div>
-        );
+  constructor() {
+    super();
+    this.changePokemon = this.changePokemon.bind(this);
+    this.state = {
+      pokemonIndex: 0,
     }
+  }
+
+  changePokemon() {
+    const { pokemons } = this.props
+    console.log(pokemons.length)
+
+    if (this.state.pokemonIndex < pokemons.length - 1 ) {
+      this.setState({
+        pokemonIndex: this.state.pokemonIndex + 1,
+      })
+    } else {
+      this.setState({
+        pokemonIndex: 0,
+      })
+    }
+  }
+
+  render() {
+    const { pokemons } = this.props;
+  
+    const pokemon = pokemons
+      .find((pokemon) => pokemons.indexOf(pokemon) === this.state.pokemonIndex);
+
+    const { name, type, averageWeight, image } = pokemon;
+
+    return (
+      <div>
+        <div className="pokemon">
+          <div>
+            <p> {name} </p>
+            <p> {type} </p>
+            <p> {`Average weight: ${averageWeight.value} ${averageWeight.measurementUnit}`}</p>
+          </div>
+          <img src={image} alt={`${name} sprite`} />
+        </div>
+        <button onClick={ this.changePokemon }>Next Pokemon</button>
+      </div>
+    );
+  }
 }
 
 export default Pokemon;
