34e34290424c8b0c9fcef05b6e47de524bfebb64
felipe_louzeiro <fl.louzeiro@gmail.com>
2021-07-01 11:33:59 -0300

    changes

---
+++ src/components/Input.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class Input extends Component {
+  render() {
+    return(
+      <span>Input</span>
+    )
+  }
+}
+
+export default Input;
--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -2,22 +2,11 @@ import React from 'react';
 import Pokemon from './Pokemon';
 
 class Pokedex extends React.Component {
-    constructor() {
-        super()
-
-        this.state = {
-            
-        }
-    }
     render() {
         return (
-            <div>
-                <input type='text' onChange={(event) => event.target.value}/>
-                <div className="pokedex">
-                    {this.props.pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
-                </div>
+            <div className="pokedex">
+                {this.props.pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
             </div>
-
         );
     }
 }
